f10031c21147ed597406ae6a2fc2ccc22fd340f2
Lene Omdal <lene.omdal@hotmail.com>
2022-03-15 13:48:20 +0100

    :sparkles: Add hierarchical clustering example

---
+++ Code/Hclust-example.R
@@ -0,0 +1,25 @@
+# Hierarchical clustering example
+library(ggplot2)
+# Simulate data
+sim.norm.df = data.frame(x = rnorm(10), y = rnorm(10))
+plot(sim.norm.df)
+
+# cluster simulated data
+hclust.sim = hclust(dist(sim.norm.df), method = "ward.D2")
+
+# save plot of dendrogram
+svg("C:\\Users\\Lene\\repositories\\master-thesis-latex\\figures\\hclust-example.svg")
+plot(hclust.sim, main = NULL, sub = NA, xlab = "", ylab = "")
+dev.off()
+
+
+sim.norm.df$clust.num = 1:10
+# save plot of simulated data points
+svg("C:\\Users\\Lene\\repositories\\master-thesis-latex\\figures\\points-hclust.svg")
+ggplot(data = sim.norm.df, aes(x = x, y = y, label = clust.num)) +
+  ylim(c(-1.5, 1.5)) + xlim(-1.5,1.5) + geom_text(size = 5.5) + 
+  theme(axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),  
+        axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14))
+dev.off()
