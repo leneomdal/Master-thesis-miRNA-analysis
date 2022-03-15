f2bc07a311e3d43634dc187fb736b8a2e6774224
Lene Omdal <lene.omdal@hotmail.com>
2022-03-15 14:15:50 +0100

    :seedling: Add seed to simalted example

--- Code/Hclust-example.R
+++ Code/Hclust-example.R
@@ -1,6 +1,7 @@
 # Hierarchical clustering example
 library(ggplot2)
 # Simulate data
+set.seed(123)
 sim.norm.df = data.frame(x = rnorm(10), y = rnorm(10))
 plot(sim.norm.df)
 
@@ -17,7 +18,7 @@ sim.norm.df$clust.num = 1:10
 # save plot of simulated data points
 svg("C:\\Users\\Lene\\repositories\\master-thesis-latex\\figures\\points-hclust.svg")
 ggplot(data = sim.norm.df, aes(x = x, y = y, label = clust.num)) +
-  ylim(c(-1.5, 1.5)) + xlim(-1.5,1.5) + geom_text(size = 5.5) + 
+  ylim(c(-2, 2)) + xlim(-2,2) + geom_text(size = 5.5) + 
   theme(axis.text.x = element_text(size = 14),
         axis.text.y = element_text(size = 14),  
         axis.title.x = element_text(size = 14),
