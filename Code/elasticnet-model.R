3f51938ba3e9d8c4df29398dc8933ba79aa602f1
Lene Omdal <lene.omdal@hotmail.com>
2022-03-21 17:31:26 +0100

    :construction: Include bootstrap of elasticnet coeffs

--- Code/elasticnet-model.R
+++ Code/elasticnet-model.R
@@ -2,12 +2,13 @@ library(glmnet)
 library(caret)
 library(foreach)
 library(itertools)
+library(boot)
 
 # GLMNET
-log.cpm.df = as.data.frame(t(log.cpm))
+log.cpm.ad = as.data.frame(t(log.cpm))
 ad= metadata.df$ad
-log.cpm.df[,"AD"] = ad
-mod.matrix = model.matrix(AD~., data = log.cpm.df)
+log.cpm.ad[,"AD"] = ad
+mod.matrix = model.matrix(AD~., data = log.cpm.ad)
 
 #Define elsatic net model using alpha = 0.5
 enet.mod = glmnet(mod.matrix, y = ad, alpha = 0.5, family = "binomial", 
@@ -21,7 +22,6 @@ set.seed(50)
 cv.enet = cv.glmnet(mod.matrix, y = ad, alpha = 0.5, family = "binomial", 
                     standardize = TRUE, nfolds = 5, type.measure = "dev")
 plot(cv.enet)
-?plot.cv.glmnet
 cv.enet$cvm
 
 
@@ -45,63 +45,72 @@ lambdas = seq(0,1)
 
 
 # cross validate lambdas  NEEED TO DEFINE A VECTOR OF LAMBDAS HERE!!!! DEFINE FOLDID, SAME FOLDS FOR EACH ALPHA
-cv.df = data.frame(alpha = rep(0, length(alphas)), mcve = rep(0,length(alphas)), lambda = rep(0,length(alphas)))
-View(cv.df)
-for(i in 1:length(alphas)){
-  cv = cv.glmnet(mod.matrix, ad, type.measure = "deviance", family = "binomial", 
-                 standardize = TRUE, alpha = alphas[i], foldid = folds)
-  cv.df[i,] = c(alphas[i], cv$cvm[cv$lambda ==cv$lambda.min], cv$lambda.min)
-}
-
-alpha.min = cv.df$alpha[cv.df$mcve == min(cv.df$mcve)]
-lambda.min = cv.df$lambda[cv.df$mcve == min(cv.df$mcve)] 
-
-enet.fit = glmnet(mod.matrix, ad, family = "binomial", standardize = TRUE, 
-                  lambda = lambda.min, alpha = alpha.min)
-enet.fit$lambda
-coef(enet.fit)
-
-
+# cv.df = data.frame(alpha = rep(0, length(alphas)), mcve = rep(0,length(alphas)), lambda = rep(0,length(alphas)))
+# View(cv.df)
+# for(i in 1:length(alphas)){
+#   cv = cv.glmnet(mod.matrix, ad, type.measure = "deviance", family = "binomial", 
+#                  standardize = TRUE, alpha = alphas[i], foldid = folds)
+#   cv.df[i,] = c(alphas[i], cv$cvm[cv$lambda ==cv$lambda.min], cv$lambda.min)
+# }
+# 
+# alpha.min = cv.df$alpha[cv.df$mcve == min(cv.df$mcve)]
+# lambda.min = cv.df$lambda[cv.df$mcve == min(cv.df$mcve)] 
+# 
+# enet.fit = glmnet(mod.matrix, ad, family = "binomial", standardize = TRUE, 
+#                   lambda = lambda.min, alpha = alpha.min)
+# enet.fit$lambda
+# coef(enet.fit)
 
 
-#Nested cross validation of alpha
-set.seed(5)
-n.folds = 5
-folds.alpha = sample(rep(1:n.folds, 12), 60, replace = FALSE)
-folds.lambda = sample(rep(1:n.folds, 12), 60 - 12, replace = FALSE)
-
-
+# Function for calculating deviance
 find.dev = function(pred, truth){
   dev = -2*(truth %*% log(pred) + (1 - truth) %*% log(1 - pred))
   return(dev)
 }
 
-set.seed(50)
-cv.alpha.df = data.frame(alpha = rep(NA,length(alphas)), deviance = rep(NA, length(alphas)))
-for(j in seq_along(alphas)){
-  dev = 0
-  for(i in 1:n.folds){
-    curr.test.fold = folds.alpha == i
-    curr.train.fold = folds.alpha != i
-    test.set = mod.matrix[curr.test.fold,]
-    train.set = mod.matrix[curr.train.fold, ]
-    cv.fit = cv.glmnet(train.set, ad[curr.train.fold], family = "binomial", 
-                       standadize = TRUE, alpha = a, foldid = folds.lambda )
-    lambda.se = cv.fit$lambda.1se
-    best.fit = glmnet(train.set, ad[curr.train.fold], family = "binomial", alpha = alphas[j],
-                      lambda = lambda.se, foldid = folds.lambda)
-    pred = predict(best.fit, test.set, s = lambda.se, type = "response")
-    dev = dev + find.dev(pred, ad[curr.test.fold])
+
+#Define number of folds for nested CV of lambda and alpha
+n.folds = 5
+#Function for nested cv NEEDS INPUT VECTOR OF LAMBDAS
+nested.cv.alpha = function(n.folds, alphas, lambda.type = "lambda.1se"){
+  folds.alpha = sample(rep(1:n.folds, 12), nrow(mod.matrix), replace = FALSE)
+  folds.lambda = sample(rep(1:n.folds, 12), nrow(mod.matrix) - (nrow(mod.matrix)/n.folds), 
+                        replace = FALSE)
+  cv.alpha.df = data.frame(alpha = rep(NA,length(alphas)), deviance = rep(NA, length(alphas)))
+  
+  for(j in seq_along(alphas)){
+    dev = 0
+    for(i in 1:n.folds){
+      curr.test.fold = folds.alpha == i
+      curr.train.fold = folds.alpha != i
+      test.set = mod.matrix[curr.test.fold,]
+      train.set = mod.matrix[curr.train.fold, ]
+      cv.fit = cv.glmnet(train.set, ad[curr.train.fold], family = "binomial", 
+                         standadize = TRUE, alpha = a, foldid = folds.lambda )
+      if(lambda.type == "lambda.1se"){
+        lambda = cv.fit$lambda.1se
+      }
+      else if(lambda.type == "lambda.min"){
+        lambda = cv.fit$lambda.min
+      }
+      else{
+        print("Choose either lambda.min or lambda.1se for regularization")
+      }
+      best.fit = glmnet(train.set, ad[curr.train.fold], family = "binomial", alpha = alphas[j],
+                        lambda = lambda, foldid = folds.lambda)
+      pred = predict(best.fit, test.set, s = lambda, type = "response")
+      dev = dev + find.dev(pred, ad[curr.test.fold])
+    }
+    cv.alpha.df[j,] = c(alphas[j], dev/nrow(mod.matrix))
   }
-  cv.alpha.df[j,] = c(alphas[j], dev/nrow(mod.matrix))
+  return(cv.alpha.df)
 }
-View(cv.alpha.df)
 
+set.seed(50)
+nested.cv.alpha(n.folds, alphas, lambda.type = "lambda.1se")
 
-#Find alpha min
+#Find alpha min from nested cv
 alpha.min = cv.alpha.df$alpha[cv.alpha.df$deviance == min(cv.alpha.df$deviance)]
-
-
 # Cross validate for lambdas using alpha min on entire data set
 cv.enet = cv.glmnet(mod.matrix, ad, family = "binomial", alpha = alpha.min)
 plot(cv.enet)
@@ -110,5 +119,36 @@ lambda.se = cv.enet$lambda.1se
 
 enet.mod = glmnet(mod.matrix, ad, family = "binomial", alpha = alpha.min, 
                   lambda = lambda.se)
-coeffs = coef(cv.enet, s = lambda.se)
+coeffs = coef(enet.mod)
 included.coeffs = as.matrix(coeffs)[as.vector(coeffs) != 0,]
+
+
+# Paired bootstrap
+?boot
+
+bootstrap.elasticnet = function(n.boot, alpha, lambda){
+  included.coeffs =c()
+  for(i in 1:n.boot){
+    boot.index = sample(1:ncol(log.cpm), size = ncol(log.cpm), replace = TRUE)
+    boot.df = log.cpm.ad[boot.index, ]
+    
+    model.mat = model.matrix(AD~., data = boot.df)
+    elasticnet.mod.boot = glmnet(model.mat, ad[boot.index], family = "binomial", 
+                                 alpha = alpha, lambda = lambda)
+    coeffs = coef(elasticnet.mod.boot)
+    included.coeffs = c(as.matrix(coeffs)[as.vector(coeffs) != 0,][-1], included.coeffs)
+    curr.coeffs = as.matrix(coeffs)[as.vector(coeffs) != 0,][-1]
+    for(j in seq_along(curr.coeffs)){
+      
+    }
+  }
+  return(included.coeffs)
+}
+coeffs.boot = bootstrap.elasticnet(100, alpha.min, lambda.se)
+
+
+
+hist(coeffs.boot[names(coeffs.boot) == "`miR-20a-3p`"])
+coeffs.boot
+
+matrix(data= c(1,2,3,4,5,6), nrow=2, ncol = 3)[]
