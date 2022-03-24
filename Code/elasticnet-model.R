57fe5cbbf30573c4d303f2bd0425cd76c0e4a766
Lene Omdal <lene.omdal@hotmail.com>
2022-03-24 14:45:50 +0100

    :bug: Fix correct response in bootstraped nested cv

--- Code/elasticnet-model.R
+++ Code/elasticnet-model.R
@@ -3,6 +3,7 @@ library(caret)
 library(foreach)
 library(itertools)
 library(boot)
+library(tictoc)
 source("Code//normalize-and-filter.R")
 
 #Define data frame including ad response
@@ -12,8 +13,13 @@ log.cpm.ad[,"AD"] = ad
 #Define model matrix for glmnet model
 mod.matrix = model.matrix(AD~., data = log.cpm.ad)
 full.mod.matrix = model.matrix(AD~., data = log.cpm.ad)
-
+View(test.mat)
 # GLMNET tesing
+
+set.seed(51)
+train.ind = sample(1:50, 50, replace = FALSE)
+train.mat = mod.matrix[train.ind,]
+test.mat = mod.matrix[c(51:60),]
 
 #Define elastic net model using alpha = 0.5
 enet.mod = glmnet(mod.matrix, y = ad, alpha = 0.5, family = "binomial", 
@@ -21,10 +27,15 @@ enet.mod = glmnet(mod.matrix, y = ad, alpha = 0.5, family = "binomial",
 plot(enet.mod, xvar = "dev")
 
 # Cross validation
-set.seed(50)
-cv.enet = cv.glmnet(mod.matrix, y = ad, alpha = 0.5, family = "binomial", 
+set.seed(51)
+cv.enet = cv.glmnet(train.set, y = ad[curr.train.fold], alpha = 0.1, family = "binomial", 
                     standardize = TRUE, nfolds = 5, type.measure = "dev")
+
 plot(cv.enet)
+
+pred = predict(cv.enet, test.mat, s = "lambda.min",type = "response")
+tru = ad[51:60]
+
 
 best.lambda = cv.enet$lambda.min
 coefficients = coef(cv.enet, s = best.lambda)
@@ -63,63 +74,91 @@ find.dev = function(pred, truth){
   return(dev)
 }
 
+# Functions for testing speed of code
+tic = function() {
+  .GlobalEnv$tictoc_var = Sys.time()
+}
+toc = function() {
+  print( Sys.time() - tictoc_var)
+}
+
+lambda.grid=10^seq(1,-2,length=100)
+
 
 
 #Function for nested CV NEEDS INPUT VECTOR OF LAMBDAS
-nested.cv.alpha = function(mod.matrix, n.folds.outer, n.folds.inner, alphas, lambda.type = "lambda.1se"){
+nested.cv.alpha = function(mod.matrix, response.var, n.folds.outer, n.folds.inner, alphas, lambda.type = "lambda.min"){
   nrow.mod.matrix = nrow(mod.matrix)
-  folds.outer = sample(x = rep(1:n.folds.outer, ceiling(nrow.mod.matrix/n.folds.outer)), 
+  folds.outer = sample(x = rep(1:n.folds.outer, ceiling(nrow.mod.matrix/n.folds.outer))[1:nrow.mod.matrix], 
                        size = nrow.mod.matrix, replace = FALSE)
   list.foldid.inner = list()
   for(i in seq_len(n.folds.outer)){
     nrow.train.i = nrow.mod.matrix - sum(folds.outer == i)
-    list.foldid.inner[[i]] = sample(rep(1:n.folds.inner, times = ceiling(nrow.train.i/n.folds.inner)), 
+    list.foldid.inner[[i]] = sample(rep(1:n.folds.inner, 
+                                        times = ceiling(nrow.train.i/n.folds.inner))[1:nrow.train.i], 
                                     nrow.train.i, replace = FALSE)
   }
-  cv.alpha.df = data.frame(alpha = rep(NA,length(alphas)), deviance = rep(NA, length(alphas)))
-  
-  for(j in seq_along(alphas)){
+  cv.alpha.df = data.frame(matrix(nrow = length(alphas), ncol = 2))
+  colnames(cv.alpha.df) = c("alpha", "deviance")
+  tic()
+  for(j in seq_along(alphas)){                             # for each alpha run nested CV
     dev = 0
-    for(i in seq_len(n.folds.outer)){
+    for(i in seq_len(n.folds.outer)){                      # outer fold, nested CV 
       curr.test.fold = folds.outer == i
       curr.train.fold = folds.outer != i
       test.set = mod.matrix[curr.test.fold,]
       train.set = mod.matrix[curr.train.fold, ]
-      cv.fit = cv.glmnet(train.set, ad[curr.train.fold], family = "binomial",
-                         alpha = alphas[j], foldid = as.vector(list.foldid.inner[[i]]) )
-      pred = predict(cv.fit, newx = test.set, s = lambda.type, type = "response")
       
-      dev = dev + find.dev(pred, as.numeric(ad[curr.test.fold]))
+      #FEIL HER!!! AD MÅ TAES INN, BLIR FEIL Å BRUKE AD NÅR DENNE KJØRES MED BOOTSTRAPPED SAMPLE
+      cv.fit = cv.glmnet(train.set, response.var[curr.train.fold], family = "binomial",
+                         alpha = alphas[j], foldid = as.vector(list.foldid.inner[[i]]),
+                         standardize = TRUE)               # inner fold, CV for lambda
+      
+      pred = predict(cv.fit, newx = test.set, s = lambda.type, 
+                     type = "response")                    # predict on left out test set of CV
+      
+      dev = dev + find.dev(pred, as.numeric(as.character(response.var[curr.test.fold])))
     }
-    cv.alpha.df[j,] = c(alphas[j], dev/nrow.mod.matrix)
+    
+    cv.alpha.df[j,] = c(alphas[j], dev/nrow.mod.matrix)   # store average deviance across folds
+                                                          # together with its alpha value
   }
+  toc()
+  print("im here")
   return(cv.alpha.df)
 }
-set.seed(50)
-lol.df = nested.cv.alpha(mod.matrix, n.folds.outer, n.folds.inner, alphas)
+
+set.seed(51)
+lol.df = nested.cv.alpha(mod.matrix, ad, n.folds.outer, n.folds.inner, alphas, lambda.type = "lambda.min")
 View(lol.df)
 
+
+
 # Paired bootstrap
-bootstrap.elasticnet = function(log.cpm.ad, full.mod.matrix, n.boot, n.folds.outer, n.folds.inner, alphas, lambda.type = "lambda.1se"){
+bootstrap.elasticnet = function(log.cpm.ad, full.mod.matrix, n.boot, n.folds.outer, n.folds.inner, alphas, lambda.type = "lambda.min"){
   boot.coeffs.df = data.frame(matrix(ncol = ncol(full.mod.matrix) - 1, nrow = 0))
   colnames(boot.coeffs.df) = colnames(mod.matrix[,-1])
   boot.enet.mods.df = data.frame(matrix(ncol = 3, nrow = 0))
   colnames(boot.enet.mods.df) = c("alpha", "lambda", "deviance")
   
-  # define folds for CV of lambda after selecting alpha ( this should be equal for each bootrap sample)
+  # define folds for CV of lambda after selecting alpha ( this should be equal for each bootstrap sample)
   folds.lambda = sample(x = rep(1:n.folds.inner, ceiling(nrow(full.mod.matrix)/n.folds.inner)), 
                                       size = nrow(full.mod.matrix), replace = FALSE)
   for(i in 1:n.boot){
     boot.index = sample(1:nrow(log.cpm.ad), size = nrow(log.cpm.ad), replace = TRUE)
     boot.data = log.cpm.ad[boot.index, ]
-    model.mat = model.matrix(AD~., data = boot.data)
+    boot.mod.matrix = model.matrix(AD~., data = boot.data)
+    boot.ad = ad[boot.index]
     
-    nested.cv.df = nested.cv.alpha(model.mat, n.folds.outer, n.folds.inner, alphas, lambda.type)
+    nested.cv.df = nested.cv.alpha(boot.mod.matrix, boot.ad, n.folds.outer, n.folds.inner, alphas, lambda.type = lambda.type)
     
     
     #Find alpha with lowest mean deviance
+    nested.cv.df$alpha[nested.cv.df$deviance == min(nested.cv.df$deviance)]
     boot.alpha.best = nested.cv.df$alpha[nested.cv.df$deviance == min(nested.cv.df$deviance)]
-    boot.cv.enet = cv.glmnet(full.mod.matrix, ad, family = "binomial", alpha = boot.alpha.best, 
+    
+    # SKAL MAN BRUKE FULL.MOD.MAT HER ELLER HELE BOOTSTRAPPED DATASET??
+    boot.cv.enet = cv.glmnet(boot.mod.matrix, boot.ad, family = "binomial", alpha = boot.alpha.best, 
                              foldid = folds.lambda)
     
     if(lambda.type == "lambda.1se"){
@@ -145,12 +184,12 @@ bootstrap.elasticnet = function(log.cpm.ad, full.mod.matrix, n.boot, n.folds.out
 }
 
 #Define number of folds for nested CV of lambda and alpha
-n.folds.inner = 5
-n.folds.outer = 5
+n.folds.inner = 10
+n.folds.outer = 10
 
 #Define vector of alphas to run cross validation
 alphas =  seq(0.1, 0.9, 0.05)
-
+alpha
 
 set.seed(50)
 list.bootstrap = bootstrap.elasticnet(log.cpm.ad, full.mod.matrix, n.boot = 5, n.folds.outer, n.folds.inner, alphas)
@@ -161,3 +200,10 @@ View(list.bootstrap[[2]])
 #write.csv(list.bootstrap[[2]], file = "bootstrap-models.csv", row.names = FALSE)
 #write.csv(list.bootstrap[[1]], file = "bootstrap-coefficients.csv", row.names = FALSE)
 
+
+for(i in 1:10){
+  print(paste("list:", i))
+  for( j in 1:10){
+    print(sum(list.foldid.inner[[i]] == j))
+  }
+}
