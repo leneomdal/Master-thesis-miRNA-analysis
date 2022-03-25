66b4c0ce0315041e712ab69bf6e3818886f81607
Lene Omdal <lene.omdal@hotmail.com>
2022-03-25 13:38:44 +0100

    :bug: Fix annotation in heatmaps

--- Code/new-heatmaps.R
+++ Code/new-heatmaps.R
@@ -21,11 +21,10 @@ as.dendrogram(hclust.mirna) %>% plot(horiz = TRUE)
 groups_sex = ifelse(metadata.df$sex == 1, "boy", "girl")
 
 probiotic.groups = ifelse(metadata.df$probiotic == 1, "Probiotic", "Placebo")
-ad.groups = ifelse(metadata.df$ad == 1, "AD", "No AD")
-mad.groups = ifelse(metadata.df$matatopy == 1, "Maternal atopy", "No maternal atopy")
-
-sample.col = data.frame(Supplement = probiotic.groups, Atopic.dermatitis = ad.groups, 
-                        Maternal.atopy = mad.groups)
+ad.groups = ifelse(metadata.df$ad == 1, "AD", "No.AD")
+mad.groups = ifelse(metadata.df$matatopy == 1, "Maternal.atopy", "No.maternal.atopy")
+sample.col = data.frame(Supplement = as.factor(probiotic.groups), Atopic.dermatitis = as.factor(ad.groups), 
+                        Maternal.atopy = as.factor(mad.groups))
 rownames(sample.col) = colnames(log.cpm)
 
 # Set annotations colors
@@ -49,8 +48,8 @@ pheatmap(log.cpm,
 
 
 # calculate correlation between miRNAs and samples
-cor.mirna = cor(scale(t(log.cpm)))
-cor.samples = cor(t(scale(t(log.cpm))))
+cor.mirna = cor(scale(t(log.cpm)), method = "spearman")
+cor.samples = cor(t(scale(t(log.cpm))), method = "pearson")
 
 
 # Heatmap using cor based disance !!!! 1-cor !!!!
@@ -59,7 +58,8 @@ clust.s.cor = hclust(as.dist(1-cor.samples), method = "ward.D2")
 
 
 pheatmap(log.cpm, color = colors, scale = "row",  border_color = NA, 
-         clustering_distance_rows = "correlation", clustering_distance_cols = "correlation",
+         clustering_distance_rows = as.dist(1-(cor.mirna)), 
+         clustering_distance_cols = as.dist(1- (cor.samples)),
          clustering_method = "ward.D2", annotation_col = sample.col, show_rownames = FALSE,
          show_colnames = FALSE, annotation_colors = ann.colors,
          main = "Heatmap of log cpm values using 1 - correlation distance", fontsize = 7)
